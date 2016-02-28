d851a130edb8a7a29f6d7740ddf5fab77b1b1a77
spike <morndragon@126.com>
2016-02-28 20:36:38 +0800

    添加跳转

--- app/src/main/java/me/chunyu/spike/wcl_onboarding_demo/MainActivity.java
+++ app/src/main/java/me/chunyu/spike/wcl_onboarding_demo/MainActivity.java
@@ -1,71 +1,28 @@
 package me.chunyu.spike.wcl_onboarding_demo;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.ViewCompat;
-import android.support.v4.view.ViewPropertyAnimatorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    public static final int STARTUP_DELAY = 300; // 启动延迟
-    public static final int ANIM_ITEM_DURATION = 1000;
-    public static final int ITEM_DELAY = 300;
 
-    @Bind(R.id.onboard_ll_container) LinearLayout mLlContainer;
-    @Bind(R.id.onboard_iv_logo) ImageView mIvLogo;
-    @Bind(R.id.onboard_b_choice_1) Button mBChoice1;
-    @Bind(R.id.onboard_b_choice_2) Button mBChoice2;
-
-    private View.OnClickListener mOnClickListener = new View.OnClickListener() {
-        @Override public void onClick(View v) {
-            Toast.makeText(getApplicationContext(), "Nice!", Toast.LENGTH_SHORT).show();
-        }
-    };
+    @Bind(R.id.main_b_onboard_center) Button mBOnboardCenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_onboarding_center);
+        setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        // 向下移动
-        ViewCompat.animate(mIvLogo)
-                .translationY(-300)
-                .setStartDelay(STARTUP_DELAY)
-                .setDuration(ANIM_ITEM_DURATION)
-                .setInterpolator(new DecelerateInterpolator(1.2f))
-                .start();
-
-        for (int i = 0; i < mLlContainer.getChildCount(); i++) {
-            View v = mLlContainer.getChildAt(i);
-            ViewPropertyAnimatorCompat viewAnimator;
-
-            // TextView控件, Button是TextView的子类
-            // 从消失到显示
-            if (!(v instanceof Button)) {
-                viewAnimator = ViewCompat.animate(v)
-                        .translationY(50).alpha(1)
-                        .setStartDelay((ITEM_DELAY * i) + 500)
-                        .setDuration(1000);
-            } else { // Button控件, 从缩小到扩大
-                viewAnimator = ViewCompat.animate(v)
-                        .scaleY(1).scaleX(1)
-                        .setStartDelay((ITEM_DELAY * i) + 500)
-                        .setStartDelay(500);
+        mBOnboardCenter.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, OnboardingCenterActivity.class));
             }
-
-            viewAnimator.setInterpolator(new DecelerateInterpolator()).start();
-        }
-
-        mBChoice1.setOnClickListener(mOnClickListener);
-        mBChoice2.setOnClickListener(mOnClickListener);
+        });
     }
 }

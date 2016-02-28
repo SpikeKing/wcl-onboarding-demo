27da1331cba5bfe857291c5c11c363e4c12f123d
spike <morndragon@126.com>
2016-02-28 10:12:38 +0800

    first commit

---
+++ app/src/main/java/me/chunyu/spike/wcl_onboarding_demo/MainActivity.java
@@ -0,0 +1,13 @@
+package me.chunyu.spike.wcl_onboarding_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding_center);
+    }
+}
